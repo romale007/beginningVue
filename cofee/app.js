83302fb78d94288931073b28c0c03ae753e2327c
romale007 <romale0819@gmail.com>
2021-05-28 23:06:26 +0300

    refreshed

--- cofee/app.js
+++ cofee/app.js
@@ -1,50 +1,47 @@
 let PlanComponent = {
-    template: '#plan-template',
-    props: {
-        name: {
-            type: String,
-            default: "Some def name",
-            required: true
-        },
-        selectedPlan: {
-            type: String
-        },
+  template: '#plan-template',
+  props: {
+    name: {
+      type: String,
+      required: true
     },
-    computed: {
-        isSelected () {
-            return this.name === this.selectedPlan;
-        }
-    },
-    methods: {
-        select () {
-            this.$emit('select', this.name); // пользовательское событие, первый арг (обязателен) - имя(любое потом можно "слушать" через v-on в атрибуте)
-        }
+    selectedPlan: {
+      type: String
     }
+  },
+  computed: {
+    isSelected() {
+      return this.name === this.selectedPlan
+    }
+  },
+  methods: {
+    select() {
+      this.$emit('select', this.name) // пользовательское событие, первый арг (обязателен) - имя(любое потом можно "слушать" через v-on в атрибуте)
+    }
+  }
 }
 
 let PlanPickerComponent = {
-    template: '#plan-picker-template',
-    components: {
-        plan: PlanComponent,
-    },
-    data() {
-        return {
-            plans: ["The Single", "The Curious", "The Addict"],
-            selectedPlan: null
-        }
-    },
-    methods: {
-        selectPlan(plan) {
-            this.selectedPlan = plan
-        }
+  template: '#plan-picker-template',
+  components: {
+    'plan': PlanComponent
+  },
+  data() {
+    return {
+      plans: ['The Hacker', 'The Curious', 'The Single', 'Some Plan'],
+      selectedPlan: null
     }
+  },
+  methods: {
+    selectPlan(plan) {
+      this.selectedPlan = plan
+    }
+  }
 }
 
-
-
 new Vue({
-    el: "#app",
-    components: {
-        PlanPicker: PlanPickerComponent
-    }
-});
+  el: '#app',
+  components: {
+    'plan-picker': PlanPickerComponent
+  }
+})
